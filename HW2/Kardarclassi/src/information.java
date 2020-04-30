5167021d51d4877fb940f00cda935ad70ff1558f
amirrrrr <mahdimehraban@nobody.invalid>
2020-04-30 22:42:45 +0430

    HW2

---
+++ HW2/Kardarclassi/src/information.java
@@ -0,0 +1,46 @@
+
+public class information {
+     private String Name;
+    private String Family;
+    private int Age;
+    private int Pass;
+    private int Username;
+    private String Job;
+    
+    public String getName(){
+        return Name;
+    }
+    public void setName(String Name){
+        this.Name=Name;
+    }
+    public void setFamily(String Family){
+        this.Family=Family;
+    }
+    public String getFamily(){
+        return Family;
+    }
+    public void setAge(int Age){
+        this.Age=Age;
+    }
+    public int getAge(){
+        return Age;
+    }
+    public void setPass(int Pass){
+        this.Pass=Pass;
+    }
+    public int getPass(){
+        return Pass;
+    }
+    public void setUsername(int Username){
+        this.Username=Username;
+    }
+    public int getUsername(){
+        return Username;
+    }
+    public void setJob(String Job){
+        this.Job=Job;
+    }
+    public String getJob(){
+        return Job;
+    }
+}
