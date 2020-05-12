379e126c2427a1960ac1438589a8ffc9ea8cf6a8
amirrrrr <mahdimehraban@mahdis-MacBook-Pro.local>
2020-05-12 17:17:50 +0430

    by Maghsoudi

---
+++ HW1/kar dar kelas/iff.java
@@ -0,0 +1,23 @@
+/**
+ * Created by Minoofam on 2/7/2020.
+ */
+public class iff {
+    public static void main (String[] args) {
+        int x, y;
+
+        x = 10;
+        y = 20;
+
+        if(x < y) System.out.println("x is less than y");
+
+        x = x * 2;
+        if(x == y) System.out.println("x now equal to y");
+
+        x = x * 2;
+        if(x > y) System.out.println("x now greater than y");
+
+        // this won't display anything
+        if(x == y) System.out.println("you won't see this");
+
+    }
+}
